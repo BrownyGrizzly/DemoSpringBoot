9fc6dccc246596843303eb6eb2d9adbcf539045d
HiddenGrizzly <153844656+BrownyGrizzly@users.noreply.github.com>
2024-05-09 21:15:10 +0700

    Done

--- fashion-store/src/main/java/com/fashionboutique/fashionstore/service/ProductService.java
+++ fashion-store/src/main/java/com/fashionboutique/fashionstore/service/ProductService.java
@@ -2,9 +2,11 @@ package com.fashionboutique.fashionstore.service;
 
 import com.fashionboutique.fashionstore.dto.ProductDTO;
 import com.fashionboutique.fashionstore.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ProductService {
     List<Product> getAllProducts();
     Product getProductById(Long id);
